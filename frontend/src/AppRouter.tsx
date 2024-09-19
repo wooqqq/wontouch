ad11ddbbfa237ad777978f6e917b13d17a4d0000
hhsssu <hhssuu14@naver.com>
2024-09-19 10:53:42 +0900

    Docs: prettier 파일 생성

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -1,16 +1,18 @@
 // src/AppRouter.tsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Lobby from './pages/Lobby';
-import WaitingRoom from './pages/WaitingRoom';
-import Game from './pages/Game';
-import Login from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Lobby from "./pages/Lobby";
+import WaitingRoom from "./pages/WaitingRoom";
+import Game from "./pages/Game";
+import Login from "./pages/Login";
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        {/* 게임방 입장 전 대기 (게임방 목록 페이지) */}
         <Route path="/lobby" element={<Lobby />} />
+        {/* 게임 시작 전 대기방 */}
         <Route path="/waiting-room" element={<WaitingRoom />} />
         <Route path="/game" element={<Game />} />
       </Routes>
