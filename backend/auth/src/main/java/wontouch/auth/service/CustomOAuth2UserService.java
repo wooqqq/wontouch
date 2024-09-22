05bd4371827abd5add4013cb2f86763a1983e800
wooqqq <sge0225@naver.com>
2024-09-22 23:30:18 +0900

    Feat: 카카오 유저 정보 불러오기 기능 구현

--- backend/auth/src/main/java/wontouch/auth/service/CustomOAuth2UserService.java
+++ backend/auth/src/main/java/wontouch/auth/service/CustomOAuth2UserService.java
@@ -1,13 +1,20 @@
 package wontouch.auth.service;
 
+import com.nimbusds.jose.shaded.gson.Gson;
 import com.nimbusds.jose.shaded.gson.JsonElement;
 import com.nimbusds.jose.shaded.gson.JsonParser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import wontouch.auth.dto.response.GoogleResponseDto;
 import wontouch.auth.dto.response.JwtResponseDto;
+import wontouch.auth.dto.response.KakaoResponseDto;
 import wontouch.auth.entity.Token;
 import wontouch.auth.entity.User;
 import wontouch.auth.global.exception.CustomException;
@@ -22,6 +29,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -120,6 +128,71 @@ public class CustomOAuth2UserService {
         }
 
         return null;
+    }
+
+    /**
+     * 카카오 콜백 메서드
+     */
+    public JwtResponseDto.TokenInfo kakaoCallback(String accessToken) {
+//        KakaoResponseDto kakaoResponseDto =
+
+
+        return null;
+    }
+
+    /**
+     * 카카오 유저의 정보 가져오기
+     */
+    public KakaoResponseDto getKakaoUserInfo(String accessToken) {
+        // HttpHeader 생성
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + accessToken);
+        httpHeaders.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpHeader 와 HttpBody 를 하나의 객체에 담기 (body 정보는 생략 가능)
+        HttpEntity<String> requestEntity = new HttpEntity<>(httpHeaders);
+
+        // RestTemplate 를 이용하여 HTTP 요청 처리
+        RestTemplate restTemplate = new RestTemplate();
+
+        // HTTP 요청을 GET 방식으로 실행하여 멤버 정보를 가져옴
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.GET,
+                requestEntity,
+                String.class
+        );
+
+        // 카카오 인증 서버가 반환한 사용자 정보
+        String userInfo = responseEntity.getBody();
+
+        // JSON 데이터에서 필요한 정보 추출
+        Gson gsonObj = new Gson();
+        Map<?, ?> data = gsonObj.fromJson(userInfo, Map.class);
+
+        // 이메일 동의 여부 확인
+        boolean emailAgreement = (boolean) ((Map<?, ?>) (data.get("kakao_account"))).get("email_needs_agreement");
+        String email;
+
+        if (emailAgreement) {
+            email = "";
+        } else {
+            email = (String) ((Map<?, ?>) (data.get("kakao_account"))).get("email");
+        }
+
+        // 이미 존재하는 경우
+//        if (userRepository.findByEmail(email))
+
+        // 닉네임 동의 여부 확인
+        boolean nickNameAgreement = (boolean) ((Map<?, ?>) (data.get("kakao_account"))).get("profile_nickname_needs_agreement");
+        String nickName;
+        if (nickNameAgreement) {
+            nickName = "";
+        } else {
+            nickName = (String) ((Map<?, ?>) ((Map<?, ?>) data.get("properties"))).get("nickname");
+        }
+
+        return new KakaoResponseDto(email, nickName);
     }
 
 }
