c430527937c23808c7c5965d6f90b49e568f08e4
wooqqq <sge0225@naver.com>
2024-09-23 17:17:18 +0900

    Fix: SecurityConfig 수정

--- backend/auth/src/main/java/wontouch/auth/config/SecurityConfig.java
+++ backend/auth/src/main/java/wontouch/auth/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // CSRF 비활성화
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/oauth/**").permitAll() // 해당 경로는 인증 없이 접근 가능
+                        .requestMatchers("/", "/auth/oauth/**").permitAll() // 해당 경로는 인증 없이 접근 가능
                         .anyRequest().authenticated() // 그 외의 모든 요청은 인증 필요
                 )
                 .oauth2Login(oauth2 -> oauth2
