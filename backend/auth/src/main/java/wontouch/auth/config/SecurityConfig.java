18dfe431b30f235655b1b74dbcee94567a69f8bd
wooqqq <sge0225@naver.com>
2024-09-24 10:16:14 +0900

    Fix: cors 에러 해결

--- backend/auth/src/main/java/wontouch/auth/config/SecurityConfig.java
+++ backend/auth/src/main/java/wontouch/auth/config/SecurityConfig.java
@@ -45,10 +45,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
