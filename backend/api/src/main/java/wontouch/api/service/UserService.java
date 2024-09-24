166ede5ea7d1d49d8097da89ecdeafffe34d6c49
wooqqq <sge0225@naver.com>
2024-09-24 14:59:36 +0900

    Feat: refresh token을 통한 사용자 이메일 조회 기능 구현

--- backend/api/src/main/java/wontouch/api/service/UserService.java
+++ backend/api/src/main/java/wontouch/api/service/UserService.java
@@ -1,6 +1,7 @@
 package wontouch.api.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import wontouch.api.domain.UserProfile;
@@ -8,11 +9,14 @@ import wontouch.api.dto.UserDto;
 import wontouch.api.dto.UserProfileCreateRequestDto;
 import wontouch.api.repository.UserProfileRepository;
 
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final RestTemplate restTemplate;
+    private final RedisTemplate<String, String> redisTemplate6378;
     private final UserProfileRepository userProfileRepository;
 
     public UserDto getUserDto(int userId) {
@@ -30,4 +34,18 @@ public class UserService {
 
         return userProfileRepository.save(userProfile);
     }
+
+    // refresh token으로 이메일을 찾는 메서드
+    private String findEmailByRefreshToken(String refreshToken) {
+        Map<Object, Object> tokensMap = redisTemplate6378.opsForHash().entries("refresh_token");
+
+        for (Map.Entry<Object, Object> entry : tokensMap.entrySet()) {
+            if (entry.getValue().equals(refreshToken)) {
+                return (String) entry.getKey();
+            }
+        }
+
+        return null; // 해당 refresh token 이 없는 경우
+    }
+
 }
