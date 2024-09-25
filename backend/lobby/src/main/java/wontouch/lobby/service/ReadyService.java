afb84c61bfaf6aa505915ba50130967b40d16164
leejongmin <rjsqktod1@gmail.com>
2024-09-25 13:13:31 +0900

    Feat: 시작 요청 시 참가할 플레이어 반환
    모두 준비가 되어 게임 시작을 요청했을 때 다시 한번 모두
    준비되었는지 검증한 후 실제 참가할 플레이어를 반환한다.
    반환 전에는 로비 서버에 존재하는 게임방 정보들을 redis에서 삭제한다.
    
    Signed-off-by: leejongmin <rjsqktod1@gmail.com>

--- backend/lobby/src/main/java/wontouch/lobby/service/ReadyService.java
+++ backend/lobby/src/main/java/wontouch/lobby/service/ReadyService.java
@@ -1,14 +1,18 @@
 package wontouch.lobby.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import wontouch.lobby.dto.ReadyStateDto;
 import wontouch.lobby.repository.ReadyRepository;
 
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 // 게임의 준비 시작을 담당하는 서비스 레이어
 @Service
+@Slf4j
 public class ReadyService {
 
     private final ReadyRepository readyRepository;
@@ -29,5 +33,20 @@ public class ReadyService {
         String requestUserId = kickInfo.get("requestUserId").toString();
         String playerId = kickInfo.get("playerId").toString();
         return readyRepository.kickUser(roomId, requestUserId, playerId);
+    }
+
+    public Set<String> startGameIfAllReady(String roomId) {
+        // Redis에서 준비 상태를 체크하여 모든 플레이어가 준비되었을 경우 게임을 시작
+        Optional<Set<String>> readyPlayers = readyRepository.getPlayersForGame(roomId);
+
+        if (readyPlayers.isPresent()) {
+            //모든 플레이어가 준비되었을 때 게임 시작
+            log.debug("Game can be started for players: " + readyPlayers.get());
+            return readyPlayers.get();
+        } else {
+            //플레이어가 준비되지 않았을 때
+            log.debug("Not all players are ready.");
+            throw new IllegalStateException("Not all players are ready.");
+        }
     }
 }
