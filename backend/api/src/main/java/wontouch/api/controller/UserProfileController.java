8e505ae9b40a5cbdee14dde438f5572a8bd15dc3
wooqqq <sge0225@naver.com>
2024-09-25 17:08:38 +0900

    Fix: cors 에러 해결

--- backend/api/src/main/java/wontouch/api/controller/UserProfileController.java
+++ backend/api/src/main/java/wontouch/api/controller/UserProfileController.java
@@ -1,6 +1,5 @@
 package wontouch.api.controller;
 
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
