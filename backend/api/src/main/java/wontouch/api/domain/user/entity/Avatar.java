99f80b0c74cbe82967aa9fac7d336ca7d2dfbbfc
wooqqq <sge0225@naver.com>
2024-09-26 12:09:58 +0900

    Feat: Avatar 엔티티 생성

---
+++ backend/api/src/main/java/wontouch/api/domain/user/entity/Avatar.java
@@ -0,0 +1,24 @@
+package wontouch.api.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Avatar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private int userId;
+
+    @Column(nullable = false)
+    private String characterName;
+
+}
