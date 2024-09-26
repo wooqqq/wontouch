187972acad1d6fd476723d260a35ea02209bc57f
taeseong <tskim4065@gmail.com>
2024-09-26 10:48:44 +0900

    Feat: UUID 발급 완료

--- frontend/src/components/Lobby/MakeRoom.tsx
+++ frontend/src/components/Lobby/MakeRoom.tsx
@@ -1,13 +1,25 @@
+import axios from "axios";
 import React from "react";
 
 const API_LINK = import.meta.env.VITE_API_URL;
 
-export default function MakeRoom() {
+function MakeRoom() {
+  const getUUID = async () => {
+    try {
+      const res = await axios.get(`${API_LINK}/room/create/random-uuid`);
+      console.log(res.data.data);
+    } catch (error) {
+      console.error("UUID 발급 중 에러 발생", error);
+    }
+  };
+
   return (
     <div>
       <div>
-        <button>방 생성</button>
+        <button onClick={getUUID}>방 생성</button>
       </div>
     </div>
   );
 }
+
+export default MakeRoom;
