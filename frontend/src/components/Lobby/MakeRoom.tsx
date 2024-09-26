223d9d3c1e8f04502ed58ff48a51217ffabf8664
taeseong <tskim4065@gmail.com>
2024-09-26 12:04:18 +0900

    Feat: UUID state로 관리

--- frontend/src/components/Lobby/MakeRoom.tsx
+++ frontend/src/components/Lobby/MakeRoom.tsx
@@ -1,16 +1,31 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 
 const API_LINK = import.meta.env.VITE_API_URL;
 
 function MakeRoom() {
+  // UUID 값을 state로 관리해보자
+  const [UUID, setUUID] = useState<string>("");
+  const [roomName, setRoomName] = useState<string>("");
+  const [isPrivate, setIsPrivate] = useState<boolean>(false);
+  const [password, setPassword] = useState<string>("");
+  const [showModal, setShowModal] = useState<boolean>(false);
+
   const getUUID = async () => {
     try {
       const res = await axios.get(`${API_LINK}/room/create/random-uuid`);
-      console.log(res.data.data);
+      setUUID(res.data.data);
     } catch (error) {
       console.error("UUID 발급 중 에러 발생", error);
     }
+  };
+
+  const handleMakeRoom = async () => {
+    try {
+      const res = await axios.post(`${API_LINK}/room`, {
+        roomId: UUID,
+      });
+    } catch {}
   };
 
   return (
