8286ff29f8970375fd30c6a567de59c33ae3da95
hhsssu <hhssuu14@naver.com>
2024-09-27 22:10:16 +0900

    Feat: Google 로그인 토큰 임시 발급

--- frontend/src/components/login/google/GoogleLogin.tsx
+++ frontend/src/components/login/google/GoogleLogin.tsx
@@ -1,7 +1,37 @@
+import { jwtDecode } from 'jwt-decode';
+import { setUserId } from '../../../redux/slices/userSlice';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+interface DecodedToken {
+  userId: number;
+}
+
 function GoogleLogin() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleGoogleLogin = () => {
+    const accessToken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjMsImlhdCI6MTcyNzMyODg1OSwiZXhwIjoxNzI3MzcyMDU5fQ.VdLtlr6eGo2feDkTZ3noV6YQhZWvL5RlB0jWGvqoHtg';
+
+    localStorage.setItem('access_token', accessToken);
+    const decodedToken = jwtDecode<DecodedToken>(accessToken);
+    dispatch(setUserId(decodedToken.userId));
+
+    console.log('구글 로그인 성공, Access Token:', accessToken);
+    console.log('userId: ', decodedToken.userId);
+
+    navigate('/lobby');
+  };
+
   return (
     <div>
-      <img src="src/assets/Login/google_login.png" alt="구글 로그인 이미지" />
+      <img
+        src="src/assets/Login/google_login.png"
+        alt="구글 로그인 이미지"
+        onClick={handleGoogleLogin}
+      />
     </div>
   );
 }
