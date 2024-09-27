14d6106083a85f99bc16b4073e07e91d03ce4fa7
hhsssu <hhssuu14@naver.com>
2024-09-27 22:15:02 +0900

    Feat: 웹소켓 API 연결 및 방/플레이어 정보 가져오기

--- frontend/src/components/waitingRoom/FriendInvite.tsx
+++ frontend/src/components/waitingRoom/FriendInvite.tsx
@@ -1,19 +1,19 @@
-import FriendInfo from "../common/FriendInfo";
-import CancelIcon from "../../assets/Icon/cancel.png";
-import { useState } from "react";
+import FriendInfo from '../common/FriendInfo';
+import CancelIcon from '../../assets/icon/cancel.png';
+import { useState } from 'react';
 
 // 예시 친구 데이터 (필요에 따라 props로 받거나 API로부터 가져올 수 있음)
 const friendsList = [
-  { id: 1, name: "김부르주아" },
-  { id: 2, name: "콘샐러드냠냠" },
-  { id: 3, name: "HelloWorld" },
-  { id: 4, name: "인생한방" },
-  { id: 5, name: "23토끼" },
-  { id: 6, name: "감자도리" },
+  { id: 1, name: '김부르주아' },
+  { id: 2, name: '콘샐러드냠냠' },
+  { id: 3, name: 'HelloWorld' },
+  { id: 4, name: '인생한방' },
+  { id: 5, name: '23토끼' },
+  { id: 6, name: '감자도리' },
   // 친구 수에 따라 추가
 ];
 
-function FriendInvite() {
+function FriendInvite({ onClose }: { onClose: () => void }) {
   const [invitedFriends, setInvitedFriends] = useState(
     Array(friendsList.length).fill(false), // 초기값은 모두 초대되지 않은 상태
   );
@@ -30,7 +30,10 @@ function FriendInvite() {
   return (
     <div className="yellow-box p-[37px] h-[400px] border-[#36eab5] bg-[#FFFEEE] relative">
       <div className="mint-title text-center">친구 초대</div>
-      <button className="absolute top-[10px] right-[10px] bg-none">
+      <button
+        onClick={onClose}
+        className="absolute top-[10px] right-[10px] bg-none"
+      >
         <img src={CancelIcon} alt="닫기 버튼" />
       </button>
       <section className="bg-[#E6E2C2] h-4/5 pl-[14px] pr-[30px] py-5 rounded-[10px] overflow-x-hidden overflow-y-scroll">
@@ -43,12 +46,12 @@ function FriendInvite() {
               {/* FriendInfo에 friend 데이터를 전달 */}
               <button
                 onClick={() => !invitedFriends[index] && toggleInvite(index)}
-                className={`ml-2 rounded-xl px-4 min-w-[100px] text-white ${invitedFriends ? "bg-gray-400 cursor-default" : "bg-[#896A65]"}`}
+                className={`ml-2 rounded-xl px-4 min-w-[100px] text-white ${invitedFriends ? 'bg-gray-400 cursor-default' : 'bg-[#896A65]'}`}
                 style={{
-                  pointerEvents: invitedFriends[index] ? "none" : "auto",
+                  pointerEvents: invitedFriends[index] ? 'none' : 'auto',
                 }}
               >
-                {invitedFriends[index] ? "초대완료" : "초대"}
+                {invitedFriends[index] ? '초대완료' : '초대'}
               </button>
             </div>
           ))}
--- frontend/src/components/waitingRoom/RoomTitle.tsx
+++ frontend/src/components/waitingRoom/RoomTitle.tsx
@@ -1,13 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
-function RoomTitle() {
-  const { roomId } = useParams();
+interface RoomTitleProps {
+  roomName: string;
+  roomId: string | undefined;
+}
+
+function RoomTitle({ roomName, roomId }: RoomTitleProps) {
+  const truncatedRoomId =
+    roomId && roomId.length > 3 ? roomId?.slice(0, 3) : '';
+
   return (
     <>
       {/* 방번호-제목 */}
       <div className="flex w-[418px] h-[50px] items-center bg-[#896A65] rounded-[10px] gap-2 px-[8px]">
-        <div className="room-number">{roomId}</div>
-        <div className="font-galmuri11 text-white">빨리 게임해요</div>
+        <div className="room-number">{truncatedRoomId}</div>
+        <div className="font-galmuri11 text-white">{roomName}</div>
       </div>
     </>
   );
