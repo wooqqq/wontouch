1c8d89d29ed7dc64fc141517573cce365392baad
우혜지 <sge0225@naver.com>
2024-09-30 09:06:35 +0900

    remove: 필요없는 파일 제거

--- backend/point/src/main/java/wontouch/point/domain/model/repository/MileageLogRepository.java
+++
@@ -1,9 +0,0 @@
-package wontouch.point.domain.model.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import wontouch.point.domain.entity.MileageLog;
-
-@Repository
-public interface MileageLogRepository extends MongoRepository<MileageLog, Integer> {
-}
--- backend/point/src/main/java/wontouch/point/domain/model/repository/TierPointLogRepository.java
+++
@@ -1,9 +0,0 @@
-package wontouch.point.domain.model.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import wontouch.point.domain.entity.TierPointLog;
-
-@Repository
-public interface TierPointLogRepository extends MongoRepository<TierPointLog, Integer> {
-}
