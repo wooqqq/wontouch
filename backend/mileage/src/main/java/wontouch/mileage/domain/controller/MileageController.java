5ac5493454b77331fed9f08a9db793a129b55d4e
wooqqq <sge0225@naver.com>
2024-09-30 11:02:02 +0900

    Feat: 공통 ResponseDto 생성

--- backend/mileage/src/main/java/wontouch/mileage/domain/controller/MileageController.java
+++ backend/mileage/src/main/java/wontouch/mileage/domain/controller/MileageController.java
@@ -1,4 +1,25 @@
 package wontouch.mileage.domain.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wontouch.mileage.domain.model.service.MileageService;
+
+@RestController
+@RequestMapping("/mile-log")
+@RequiredArgsConstructor
 public class MileageController {
+
+    private final MileageService mileageService;
+
+    // 마일리지 생성
+
+    // 마일리지 적립 목록 조회
+
+    // 총 마일리지 조회
+
+    // 마일리지 사용
+
+    // 마일리지 삭제
+
 }
---
+++ backend/mileage/src/main/java/wontouch/mileage/domain/model/service/MileageService.java
@@ -0,0 +1,23 @@
+package wontouch.mileage.domain.model.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import wontouch.mileage.domain.model.repository.MileageLogRepository;
+
+@Service
+@RequiredArgsConstructor
+public class MileageService {
+
+    private final MileageLogRepository mileageLogRepository;
+
+    // 마일리지 생성
+
+    // 마일리지 적립 목록 조회
+
+    // 총 마일리지 조회
+
+    // 마일리지 사용
+
+    // 마일리지 삭제
+
+}
