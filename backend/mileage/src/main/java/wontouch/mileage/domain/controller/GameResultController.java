ff0432a692b2dea3f2144d633854c9342fcd832d
wooqqq <sge0225@naver.com>
2024-10-02 13:55:38 +0900

    Feat: 마일리지 및 티어 포인트 통합 적립 구현

--- backend/mileage/src/main/java/wontouch/mileage/domain/controller/GameResultController.java
+++ backend/mileage/src/main/java/wontouch/mileage/domain/controller/GameResultController.java
@@ -1,2 +1,53 @@
-package wontouch.mileage.domain.controller;public class GameResultController {
+package wontouch.mileage.domain.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wontouch.mileage.domain.dto.request.GameResultRequestDto;
+import wontouch.mileage.domain.dto.request.MileageCreateRequestDto;
+import wontouch.mileage.domain.dto.request.TierPointCreateRequestDto;
+import wontouch.mileage.domain.model.service.MileageService;
+import wontouch.mileage.domain.model.service.TierPointService;
+import wontouch.mileage.global.dto.ResponseDto;
+
+@RestController
+@RequestMapping("/result")
+@RequiredArgsConstructor
+public class GameResultController {
+
+    private final MileageService mileageService;
+    private final TierPointService tierPointService;
+
+    /**
+     * 게임 종료 시 마일리지 및 티어 포인트 한번에 적립
+     */
+    @PostMapping
+    public ResponseEntity<?> earnPoint(@RequestBody GameResultRequestDto requestDto) {
+
+        MileageCreateRequestDto mileageDto = MileageCreateRequestDto.builder()
+                .userId(requestDto.getUserId())
+                .amount(requestDto.getMileage())
+                .build();
+        mileageService.createMileage(mileageDto);
+
+        TierPointCreateRequestDto tierPointDto = TierPointCreateRequestDto.builder()
+                .userId(requestDto.getUserId())
+                .amount(requestDto.getTierPoint())
+                .build();
+        tierPointService.createTierPoint(tierPointDto);
+
+        ResponseDto<String> responseDto = ResponseDto.<String>builder()
+                .status(HttpStatus.CREATED.value())
+                .message("마일리지 및 티어 포인트 적립 성공")
+                .data(null)
+                .build();
+
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+
 }
