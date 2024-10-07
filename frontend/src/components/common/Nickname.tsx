26c118aab495f947d798f0c2405ba6fa0e678b29
taeseong <tskim4065@gmail.com>
2024-10-07 11:27:36 +0900

    Feat: 일반방, 비밀방 입장 구현

--- frontend/src/components/common/Nickname.tsx
+++ frontend/src/components/common/Nickname.tsx
@@ -8,7 +8,7 @@ export default function Nickname() {
   const userTierPoint = useSelector((state: RootState) => state.user.tierPoint);
 
   return (
-    <div className="flex items-center brown-box w-48 h-12 justify-between px-4">
+    <div className="flex items-center brown-box w-56 h-12 justify-between px-4">
       <span className="white-text text-2xl">{userNickname}</span>
       <div className="w-8">
         <LevelImg tierPoint={userTierPoint ? userTierPoint : 0} />
