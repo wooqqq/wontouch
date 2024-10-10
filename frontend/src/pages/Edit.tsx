e0e5809f911742ff13fd03a02e0e0591a05efb98
hhsssu <hhssuu14@naver.com>
2024-10-10 09:55:18 +0900

    Refactor: 헤더 삽입 위치 변경

--- frontend/src/pages/Edit.tsx
+++ frontend/src/pages/Edit.tsx
@@ -1,12 +1,27 @@
 import EditProfile from '../components/edit/EditProfile';
 import EditCharacter from '../components/edit/EditCharacter';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Header from '../components/common/Header';
 
+import arrow from '../assets/icon/arrow_left.png';
+
 function Edit() {
+  const navigate = useNavigate();
+
+  // 뒤로 가기
+  const clickArrow = () => {
+    navigate('/lobby');
+  };
   return (
     <>
-      <Header />
+      <div className="flex items-center justify-between">
+        <button onClick={clickArrow}>
+          <img src={arrow} alt="뒤로가기" />
+        </button>
+        <div>
+          <Header />
+        </div>
+      </div>
       <Routes>
         <Route path="profile" element={<EditProfile />} />
         <Route path="character" element={<EditCharacter />} />
